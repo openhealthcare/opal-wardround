d6171ee70019139cf5200c3d9b502d8c47f4ce1e
David Miller <david@deadpansincerity.com>
2015-05-23 21:10:22 +0100

    We fetch user profiles from the .profile attribute now !
    refs openhealthcare/elcid#637

--- wardround/templatetags/wardrounds.py
+++ wardround/templatetags/wardrounds.py
@@ -8,6 +8,6 @@ register = template.Library()
 @register.inclusion_tag('wardround/partials/team_filter.html',
                         takes_context=True)
 def team_filter(context):
-    teams = context['request'].user.get_profile().get_teams()
+    teams = context['request'].user.profile.get_teams()
     teams = sorted(teams, key=lambda t: t.name)
     return {'teams': teams }
