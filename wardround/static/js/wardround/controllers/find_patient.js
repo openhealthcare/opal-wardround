1bbb037f150e25ea2c0adec569b820f4dd64542a
David Miller <david@deadpansincerity.com>
2015-06-02 13:38:45 +0100

    Less logging and more defensive coding for data.
    refs #19

--- wardround/static/js/wardround/controllers/find_patient.js
+++ wardround/static/js/wardround/controllers/find_patient.js
@@ -20,10 +20,10 @@ angular.module('opal.wardround.controllers').controller(
 
         $scope.get_filtered_episodes = function(){
             return _.filter($scope.episodes, function(e){
+                // This shouldn't happen, but it's been seen in the wild!
+                if(!e.demographics){ return false }
                 var demographics = e.demographics[0];
                 var query = $scope.filter.query.toLowerCase()
-                console.log(demographics.name)
-                console.log(query)
                 if(
                     demographics.name.toLowerCase().indexOf(query) != -1 || 
                         demographics.hospital_number.toLowerCase().indexOf(query) != -1 
