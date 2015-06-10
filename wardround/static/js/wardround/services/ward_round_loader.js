bb5fbdf05ebbeba424642a081db43f9b97493587
David Miller <david@deadpansincerity.com>
2015-06-10 19:31:25 +0100

    Use the record loader to ensure we have our schema available.
    refs openhealthcare/elcid#660

--- wardround/static/js/wardround/services/ward_round_loader.js
+++ wardround/static/js/wardround/services/ward_round_loader.js
@@ -1,17 +1,19 @@
 angular.module('opal.wardround.services')
-    .factory('wardRoundLoader', function($q, $route, $http, Episode) {
+    .factory('wardRoundLoader', function($q, $route, $http, recordLoader, Episode) {
         return function() {
 	    var deferred = $q.defer();
-	    $http.get('/wardround/'+$route.current.params.wardround).then(
-                function(resource) {
-                    var wardround = resource.data;
-                    wardround.episodes = _.map(wardround.episodes,
-                                               function(e){return new Episode(e)} );
-		    deferred.resolve(wardround);
-	        }, function() {
-		    // handle error better
-		    alert('Ward Round could not be loaded');
-	        });
+            recordLoader.then(function(records){
+	        $http.get('/wardround/'+$route.current.params.wardround).then(
+                    function(resource) {
+                        var wardround = resource.data;
+                        wardround.episodes = _.map(wardround.episodes,
+                                                   function(e){return new Episode(e)} );
+		        deferred.resolve(wardround);
+	            }, function() {
+		        // handle error better
+		        alert('Ward Round could not be loaded');
+	            });
+            });
 	    return deferred.promise;
         };
     });
