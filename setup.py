5c73c78ae3aa5d241c187838612d8ea6773ac8da
David Miller <david@deadpansincerity.com>
2016-08-19 16:10:40 +0100

    Update setup to be installablwe

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 import os
 from setuptools import setup
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+long_desc = """
+OPAL Wardround is a plugin for the OPAL web framework that provides virtual wardround and MDT
+functionality.
+
+Source code and documentation available at https://github.com/openhealthcare/opal-wardround/
+"""
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -12,8 +17,8 @@ setup(
     packages=['wardround'],
     include_package_data=True,
     license='GPL3',
-    description='OPAL Plugin for (Virtual) Ward Rounds',
-    long_description=README,
+    description='OPAL Plugin for (Virtual) Ward Rounds and MDTs',
+    long_description=long_desc,
     url='http://opal.openhealthcare.org.uk/',
     author='Open Health Care UK',
     author_email='hello@openhealthcare.org.uk',
