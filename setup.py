61cb7689c7202763a5eb6138fe47b1b4b2e47c9e
David Miller <david@deadpansincerity.com>
2016-08-19 16:05:39 +0100

    update setup for new version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='opal-wardround',
-    version='0.7.0',
+    version='0.7.1',
     packages=['wardround'],
     include_package_data=True,
     license='GPL3',
