b1aba81cdf8065d60ccc338959056115ac2ac9c5
David Miller <david@deadpansincerity.com>
2017-01-04 15:21:31 +0000

    Uncomment tests

--- wardround/tests/test_api.py
+++ wardround/tests/test_api.py
@@ -7,11 +7,11 @@ __all__ = ["TestWardround"]
 
 
 class TestAPI(OpalTestCase):
-    # def test_wardround_view(self):
-    #     url = reverse("wardround_api_view", kwargs=dict(name="test"))
-    #     self.client.login(username=self.user.username, password=self.PASSWORD)
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, 200)
+    def test_wardround_view(self):
+        url = reverse("wardround_api_view", kwargs=dict(name="test"))
+        self.client.login(username=self.user.username, password=self.PASSWORD)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
 
     def test_find_patient_view(self):
         url = reverse("find_patient_api_view", kwargs=dict(name="test"))
