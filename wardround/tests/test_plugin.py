d1aa8c1e4cd06a88ec84c5734d6f7128f6f11c14
David Miller <david@deadpansincerity.com>
2017-02-05 20:36:15 +0000

    Update to Opal 0.8 plugin location

---
+++ wardround/tests/test_plugin.py
@@ -0,0 +1,14 @@
+"""
+Unittest for plugin
+"""
+from opal.core.test import OpalTestCase
+
+from wardround import plugin
+
+class PluginTestCase(OpalTestCase):
+    def test_javascripts(self):
+        j = plugin.WardRoundsPlugin.javascripts
+        self.assertIn(
+            'js/wardround/app.js',
+            j['opal.wardround']
+        )
