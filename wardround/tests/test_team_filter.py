d698f9cfad79ae1b68b4dc0c081c7a7dba248c83
fredkingham <fredkingham@gmail.com>
2017-10-10 22:04:16 +0100

    add it to unit tests

--- wardround/tests/test_team_filter.py
+++ wardround/tests/test_team_filter.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 """
 Unittests for wardround templatetags
 """
--- wardround/tests/test_wardrounds.py
+++ wardround/tests/test_wardrounds.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 import copy
 from mock import MagicMock
 from datetime import date
